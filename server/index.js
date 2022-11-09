76197061e6bbe17a9509fc5b120305b0120470fc
Fernando <gamboaf011@gmail.com>
2022-11-08 20:41:43 -0800

    updated loader.io info

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const router = require('./routes.js');
 const path = require("path");
 const app = express();
 // declaring PORT
-let PORT = 3001;
+let PORT = 3000;
 // add DB
 const db = require('./database/db.js');
 
